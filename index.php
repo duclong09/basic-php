8ce0d44647ed91d06214aab975cf24b39539381f
duclonggitnore <duclongsew@gmail.com>
2021-03-03 10:16:58 +0700

    PHP Variables + Data Types

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <!--Variables: Biến  -->
+    <?php
+
+    $n_Name = "Long";
+    $a_Age = 21;
+    echo "There once was a man named $n_Name <br>";
+    echo "He was $a_Age years old <br>";
+    echo "He really liked the name $n_Name <br>";
+    echo "But didn't like being  $a_Age <br>";
+    echo "<hr>"
+
+    ?>
+
+    <!-- Data TypeL: Loại dữ liệu -->
+     <?php
+        $phrase = "to be or not to be";
+        $age = 21;
+        $gpa = 21.1;
+        $isMale = false;
+        
+        echo $phrase;
+     ?>
+
+
+</body>
+
+</html>
