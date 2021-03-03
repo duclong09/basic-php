a9d82736dba9ab08ed767eb9813d425ee07c696c
duclonggitnore <duclongsew@gmail.com>
2021-03-03 23:23:43 +0700

    array

--- array/index.php
+++ array/index.php
@@ -1,22 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-
 <body>
-    <form action="site.php" method="get">
-        <input type="number" name="num1">
-        <br>
-        <input type="number" name="num2">
-        <input type="submit">
-    </form>
+    
+    <?php
+        $listFriends = array("Long", "Nhi", "Quy", "Minh");
+        $listFriends[1] = "Duc";
+        echo $listFriends[1] . "<br>";
 
-   Answer: <?php echo $_GET["num1"] + $_GET["num2"] ?>
+        // Count: Đếm
+        echo count($listFriends);
+        
+    ?>
 </body>
-
 </html>
